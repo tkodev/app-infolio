48620c6ec61fdbc2ee2dd5083ea17636393e0a52
Tony Ko <htko89@gmail.com>
2017-04-17 21:37:38 -0400

    Add: Directory tree

--- sources/main.js
+++ sources/main.js
@@ -10,12 +10,15 @@ const dialog = electron.dialog;
 // Node Modules
 const path = require( 'path' );
 const url = require( 'url' );
+const dirTree = require( 'directory-tree' );
 
 // ****************************************
 // Variables Init
 // ****************************************
 // Keep a global reference of the window object so it doesn't get deleted.
 let mainWindow;
+let root_path;
+let root_tree;
 
 // ****************************************
 // Active States
@@ -44,14 +47,32 @@ function createWindow() {
 }
 // Emitted when electron is fully ready.
 app.on( 'ready', createWindow );
-// IPC Events
-ipc.on( 'open_root_btn', function( event ) {
+// Nav Pane
+ipc.on( 'root_open_btn', function( event ) {
   dialog.showOpenDialog( {
-    properties: [ 'openDirectory' ]
+    properties: [ 'openDirectory', 'createDirectory' ]
   }, function( files ) {
-    if ( files ) event.sender.send( 'opened_root', files )
+    if ( files && files.length === 1 ) {
+      root_path = files[ 0 ];
+      event.sender.send( 'root_tree', getTree( root_path ) );
+    }
   } );
 } );
+ipc.on( 'root_refresh_btn', function( event ) {
+  if ( root_path !== "" ) {
+    event.sender.send( 'root_tree', getTree( root_path ) );
+  }
+} );
+
+function getTree( path ) {
+  let tree = dirTree( path, [ '/' ] );
+  tree = {
+    'core': {
+      'data': tree
+    }
+  };
+  return tree;
+}
 
 // ****************************************
 // Other States
--- sources/renderer.js
+++ sources/renderer.js
@@ -9,16 +9,20 @@ const ipc = electron.ipcRenderer;
 // Active States
 // ****************************************
 // Document Ready
-// document.addEventListener( "DOMContentLoaded", function() {
-$( document ).ready(function() {
-  // open_root
-  const open_root_btn = document.getElementById( 'open_root_btn' );
-  open_root_btn.addEventListener( 'click', function( event ) {
-    ipc.send( 'open_root_btn' );
-    console.log("hello");
-  } )
-  ipc.on( 'opened_root', function( event, path ) {
-    // document.getElementById('selected-file').innerHTML = `You selected: ${path}`
-    console.log( path );
-  } )
+$( document ).ready( function() {
+  // Nav Pane
+  const root_open_btn = document.getElementById( 'root_open_btn' );
+  const root_refresh_btn = document.getElementById( 'root_refresh_btn' );
+  root_open_btn.addEventListener( 'click', function( event ) {
+    ipc.send( 'root_open_btn' );
+    console.log( 'root_open_btn' );
+  } );
+  root_refresh_btn.addEventListener( 'click', function( event ) {
+    ipc.send( 'root_refresh_btn' );
+    console.log( 'root_refresh_btn' );
+  } );
+  ipc.on( 'root_tree', function( event, root_tree ) {
+    console.log( root_tree );
+    $( '#nav' ).jstree( root_tree );
+  } );
 } );
