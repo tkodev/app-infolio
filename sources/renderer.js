ee5cbecccddb555f6d14e00c366e6992022b0ca1
Tony Ko <htko89@gmail.com>
2017-04-19 02:43:17 -0400

    Add: Prototype tree element rendering

--- sources/renderer.js
+++ sources/renderer.js
@@ -2,44 +2,50 @@
 // Modules Init
 // ****************************************
 // Electron Modules
-const electron = require( 'electron' );
+const electron = require( "electron" );
 const ipc = electron.ipcRenderer;
 
 // ****************************************
 // Active States
 // ****************************************
-// Global Functions
-function traverse(node,func,depth){
-  console.log(node.name);
-  if(node.children){
-    if (typeof depth === 'number'){
-      depth++;
-    } else {
-      depth = 1;
-    }
-    node.children.forEach(function(val){
-      traverse(val,func,depth);
-    });
-  }
-}
 // Document Ready
+var rootTreeNavHtml;
+
 $( document ).ready( function() {
   // Tree Pane
-  const rootOpenBtn = document.getElementById( 'rootOpenBtn' );
-  const rootRefBtn = document.getElementById( 'rootRefBtn' );
-  rootOpenBtn.addEventListener( 'click', function( event ) {
-    ipc.send( 'rootOpenEvent' );
-    console.log( 'rootOpenEvent' );
+  const rootOpenBtn = document.getElementById( "rootOpenBtn" );
+  const rootRefBtn = document.getElementById( "rootRefBtn" );
+  rootOpenBtn.addEventListener( "click", function( event ) {
+    ipc.send( "rootOpenEvent" );
+    console.log( "rootOpenEvent" );
   } );
-  rootRefBtn.addEventListener( 'click', function( event ) {
-    ipc.send( 'rootRefEvent' );
-    console.log( 'rootRefEvent' );
+  rootRefBtn.addEventListener( "click", function( event ) {
+    ipc.send( "rootRefEvent" );
+    console.log( "rootRefEvent" );
   } );
-  ipc.on( 'rootTreeEvent', function( event, rootTree ) {
+  ipc.on( "rootTreeEvent", function( event, rootTree ) {
     console.log( rootTree );
-    traverse(rootTree, renderTreeElement);
+    $( "#rootTreeNav" ).empty();
+    rootTreeNavHtml = "";
+    traverseTree( rootTree, 0 );
+    $( "#rootTreeNav" ).html( rootTreeNavHtml );
   } );
-  function renderTreeElement(){
-
+  function traverseTree( node, depth ) {
+    if ( node.name && node.name.charAt( 0 ) !== "." && !node.name.includes("node_modules")) {
+      if ( node.children.length > 0 ) {
+        rootTreeNavHtml += "<ul><li class=\"nav-group-item\"><span class=\"icon icon-down-open\"></span>" + node.name + "</li>";
+        if ( typeof depth === "number" ) {
+          depth++;
+        } else {
+          depth = 0;
+        }
+        node.children.forEach( function( val ) {
+          traverseTree( val, depth );
+        } );
+      } else {
+        rootTreeNavHtml += "<ul><li class=\"nav-group-item\"><span class=\"icon icon-folder\"></span>" + node.name + "</li>";
+      }
+      rootTreeNavHtml += "</ul>"
+    }
   }
 } );
